66fd2f381a8350f40646a27ff43a6c28060e6481
HitomaruKonpaku <hunteratthenight@gmail.com>
2020-08-07 02:28:24 +0700

    Clean URL even more

--- src/inject.js
+++ src/inject.js
@@ -1,8 +1,8 @@
 console.debug('FBCLID KILLER')
 
-const prefixToRemove = ['https://l.facebook.com/l.php?u=', 'https://www.facebook.com/']
-const paramsToRemove = ['fbclid', 'h', '__tn__', 'c[0]']
-Object.freeze(prefixToRemove)
+console.debug = () => { }
+
+const paramsToRemove = ['fbclid', '__tn__', '__cft__[0]']
 Object.freeze(paramsToRemove)
 
 const observer = new MutationObserver(mutations => {
@@ -12,15 +12,24 @@ const observer = new MutationObserver(mutations => {
       if (!node.querySelectorAll) return
       const anchors = Array
         .from(node.querySelectorAll('a'))
-        .filter(a => a.href.includes('fbclid'))
+        .filter(a => a.href)
       anchors.forEach(anchor => {
         const originHref = decodeURIComponent(anchor.href)
-        let tempHref = originHref
-        prefixToRemove.forEach(prefix => tempHref = tempHref.replace(prefix, ''))
-        const url = new URL(tempHref)
+        let tempHref = originHref.replace('https://l.facebook.com/l.php?u=', '')
+        const index = tempHref.indexOf('fbclid')
+        if (index >= 0) {
+          tempHref = tempHref.slice(0, index)
+        }
+        let url
+        try {
+          url = new URL(tempHref)
+        } catch (error) {
+          console.error(`Invalid URL '${originHref}'`)
+          return
+        }
         paramsToRemove.forEach(param => url.searchParams.delete(param))
         const newHref = url.href
-        console.debug({ new: newHref, origin: originHref })
+        console.debug({ new: newHref, org: originHref })
         anchor.href = newHref
       })
     })
